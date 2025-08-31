6817136011f8a9364a1ce6ead6f7c699f62cf09e
Lakshya <lakshyasinghaliitd@gmail.com>
2025-08-31 11:54:16 +0530

    gradient clipping, max_steps, hyperparameter

--- qwen2.py
+++ qwen2.py
@@ -87,11 +87,14 @@ class CausalSelfAttention(nn.Module):
         q = apply_rotary_pos_emb(q, cos, sin)
         k = apply_rotary_pos_emb(k, cos, sin)
 
-        attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        mask = torch.triu(torch.ones(T, T, device=x.device), diagonal=1).bool()
-        attn = attn.masked_fill(mask, float("-inf"))
-        attn = F.softmax(attn, dim=-1)
-        y = attn @ v
+        # attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        # mask = torch.triu(torch.ones(T, T, device=x.device), diagonal=1).bool()
+        # attn = attn.masked_fill(mask, float("-inf"))
+        # attn = F.softmax(attn, dim=-1)
+        # y = attn @ v
+
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+
         y = y.transpose(1, 2).contiguous().view(B, T, C)
         y = self.o_proj(y)
         return y
@@ -246,10 +249,26 @@ torch.set_float32_matmul_precision('high')
 model.eval()
 model.to(device)
 model = torch.compile(model)
-# logits, loss = model(x, y)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
-for i in range(50):
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 715
+max_steps = 19073 # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
+def get_lr(it):
+    # 1) linear warmup for warmup_iters steps
+    if it < warmup_steps:
+        return max_lr * (it+1) / warmup_steps
+    # 2) if it > lr_decay_iters, return min learning rate
+    if it > max_steps:
+        return min_lr
+    # 3) in between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
+    return min_lr + coeff * (max_lr - min_lr)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-08)
+for step in range(max_steps):
     t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
@@ -257,12 +276,18 @@ for i in range(50):
     with torch.autocast(device_type=device, dtype=torch.bfloat16):
         logits, loss = model(x, y)
     loss.backward()
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
     optimizer.step()
     torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1 - t0)*1000
     tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"step: {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
+    print(f"step: {step}| loss: {loss.item()}| lr {lr:.4e} | dt: {dt:.2f}ms| norm: {norm:.4f} | tok/sec: {tokens_per_sec:.2f}")
 import sys; sys.exit(0)
 
 max_length = 30
